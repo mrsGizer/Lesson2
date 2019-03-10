6a6b45f45c52c45a4b332dda9862d5f20b93f67c
Yuliua Pak <3511521@gmail.com>
2019-03-10 19:17:15 +0300

    в бот добавлен модуль ephem

--- 2bot.py
+++ 2bot.py
@@ -2,11 +2,23 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
 
 import ephem
+import datetime
 
 PROXY = {'proxy_url': 'socks5://t2.learn.python.ru:1080',
     'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
 
+date_now = datetime.datetime.now()
+now = date_now.strftime('%Y/%m/%d')
 
+dict_planet = {
+    'mercury': ephem.Mercury(now),
+    'venus': ephem.Venus(now),
+    'mars': ephem.Mars(now),
+    'jupiter': ephem.Jupiter(now),
+    'saturn': ephem.Saturn(now),
+    'uranus': ephem.Uranus(now),
+    'neptune': ephem.Neptune(now)
+}
 logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s', 
                     level=logging.INFO, filename='bot.log')
 
@@ -15,11 +27,21 @@ def greet_user(bot, update):
     print(text)
     update.message.reply_text(text)
 
+def planet_user(bot, update):
+    update.message.reply_text('введите название планеты на английском: ')
+    user_planet = update.message.text
+    if user_planet in dict_planet:
+        user_planet = dict_planet[user_planet]
+        text_user_planet = ephem.constellation(user_planet)
+        print(text_user_planet)
+        update.message.reply_text(text_user_planet)
+
 def main():
-    mybot = Updater("741279091:AAE_VCv1AKxkcD2v7GbqkTZfiv8B-qr6A5M", request_kwargs=PROXY)
+    mybot = Updater('741279091:AAE_VCv1AKxkcD2v7GbqkTZfiv8B-qr6A5M', request_kwargs=PROXY)
 
     dp = mybot.dispatcher
-    dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(CommandHandler('start', greet_user))
+    dp.add_handler(CommandHandler('planet', planet_user))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
     mybot.start_polling()
     mybot.idle()
